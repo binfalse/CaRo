079c9658070ac78683c9eb602b81e5d1d8c4c077
Martin Scharm <martin@binfalse.de>
2015-07-05 19:53:07 +0100

    started main

---
+++ src/main/java/de/unirostock/sems/caro/CaRo.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright © 2015 Martin Scharm <martin@binfalse.de>
+ * 
+ * This file is part of CaRo.
+ * 
+ * CaRo is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * CaRo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with CombineExt. If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.unirostock.sems.caro;
+
+/**
+ * CaRo -- converter for CombineArchives and ResearchObjects
+ * 
+ * This is the main class.
+ * 
+ * @author Martin Scharm
+ *
+ */
+public class CaRo
+{
+	
+	/**
+	 * The main method to be called by the command line.
+	 *
+	 * @param args the arguments
+	 */
+	public static void main (String[] args)
+	{
+		System.out.println ("test");
+	}
+}
